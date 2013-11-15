f0f105fbe3eae4cd5a030443405f2cbfc01d87a7
psantiago <Peter.J.Santiago@gmail.com>
2013-11-15 10:11:13 -0500

    added error validation and a class and high fives

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -1,4 +1,5 @@
 var mongodb = require('mongodb');
+var model = require('../models/entry');
 
 exports.index = function (req, res) {
   res.render('index', { title: 'Express' });
@@ -13,25 +14,31 @@ exports.postentry = function (req, res) {
     req.assert('email', 'required').notEmpty();
     req.assert('email', 'valid email required').isEmail();
     req.assert('name', '6 to 20 characters required').len(6, 20);
+    req.assert('message', '6 to 20 characters required').notEmpty().len(5, 500);
+    
     var mappedErrors = req.validationErrors(true);
     console.log(mappedErrors);
-    var entry = {
-        name: req.body.name,
-        email: req.body.email,
-        message: req.body.message,
-        approved: false,
-        date: new Date()
-    };
+    if (mappedErrors == null) {
+        var entry = model.new(
+            req.body.name,
+            req.body.email,
+            req.body.message,
+            false,
+            new Date()
+        );
 
-    var server = new mongodb.Server("itsweb-opserver.hs.wvu-ad.wvu.edu", 27017, {});
-    var client = new mongodb.Db('guestbook', server),
-        test = function (err, collection) {
-            collection.insert(entry);
-        };
+        var server = new mongodb.Server("itsweb-opserver.hs.wvu-ad.wvu.edu", 27017, {});
+        var client = new mongodb.Db('guestbook', server),
+            test = function(err, collection) {
+                collection.insert(entry);
+            };
 
-    client.open(function (err, p_client) {
-        client.collection('entries', test);
-    });
-    //req.db.push(req.body.item);
-    res.render('addentry', { title: 'Express' });
+        client.open(function(err, p_client) {
+            client.collection('entries', test);
+        });
+        res.render('addentry', { title: 'Express' });
+    } else {
+        res.render('addentry', mappedErrors);
+    }
+    
 };
---
+++ models/entry.js
@@ -0,0 +1,9 @@
+﻿exports.new = function (name, email, message, approved, date) {
+    return {
+        name: name,
+        email: email,
+        message: message,
+        approved: approved,
+        date: date
+    };
+}
