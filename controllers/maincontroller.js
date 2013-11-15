b94cee49f5d01005c51df5c3e7e3fedceb00a86d
psantiago <Peter.J.Santiago@gmail.com>
2013-11-15 09:30:38 -0500

    added simple add entry/post entry

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -1,3 +1,31 @@
-exports.index = function(req, res){
+var mongodb = require('mongodb');
+
+exports.index = function (req, res) {
   res.render('index', { title: 'Express' });
+};
+
+exports.addentry = function (req, res) {
+    res.render('addentry', { title: 'Express' });
+};
+
+exports.postentry = function (req, res) {
+    var entry = {
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        approved: false,
+        date: new Date()
+    };
+
+    var server = new mongodb.Server("itsweb-opserver.hs.wvu-ad.wvu.edu", 27017, {});
+    var client = new mongodb.Db('guestbook', server),
+        test = function (err, collection) {
+            collection.insert(entry);
+        };
+
+    client.open(function (err, p_client) {
+        client.collection('entries', test);
+    });
+    //req.db.push(req.body.item);
+    res.render('addentry', { title: 'Express' });
 };
--- server.js
+++ server.js
@@ -11,28 +11,7 @@ var express = require('express')
 
 var app = express();
 var server = new mongodb.Server("itsweb-opserver.hs.wvu-ad.wvu.edu", 27017, {});
-var client = new mongodb.Db('test', server),
-    test = function (err, collection) {
-        collection.insert({ a: 2 }, function (err, docs) {
 
-            collection.count(function (err, count) {
-                console.log(count);
-            });
-
-            // Locate all the entries using find
-            collection.find().toArray(function (err, results) {
-                //test.assertEquals(1, results.length);
-                //test.assertTrue(results[0].a === 2);
-
-                // Let's close the db
-                client.close();
-            });
-        });
-    };
-
-client.open(function(err, p_client) {
-  client.collection('test_insert', test);
-});
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -50,7 +29,8 @@ app.configure('development', function(){
 });
 
 app.get('/', maincontroller.index);
-//app.get('/users', user.list);
+app.get('/addentry', maincontroller.addentry);
+app.post('/postentry', maincontroller.postentry);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
