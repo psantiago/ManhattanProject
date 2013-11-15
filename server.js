4ba51e05c3e1aa399e96f8f242b5620e184d5ba4
psantiago <Peter.J.Santiago@gmail.com>
2013-11-15 09:03:55 -0500

    did stuff

--- server.js
+++ server.js
@@ -4,17 +4,39 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
+  , maincontroller = require('./controllers/maincontroller')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , mongodb = require('mongodb');
 
 var app = express();
+var server = new mongodb.Server("itsweb-opserver.hs.wvu-ad.wvu.edu", 27017, {});
+var client = new mongodb.Db('test', server),
+    test = function (err, collection) {
+        collection.insert({ a: 2 }, function (err, docs) {
 
+            collection.count(function (err, count) {
+                console.log(count);
+            });
+
+            // Locate all the entries using find
+            collection.find().toArray(function (err, results) {
+                //test.assertEquals(1, results.length);
+                //test.assertTrue(results[0].a === 2);
+
+                // Let's close the db
+                client.close();
+            });
+        });
+    };
+
+client.open(function(err, p_client) {
+  client.collection('test_insert', test);
+});
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'hjs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -27,8 +49,8 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', maincontroller.index);
+//app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
